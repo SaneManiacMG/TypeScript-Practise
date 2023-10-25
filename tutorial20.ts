1e14e6ebf232663429c8a853d612fa46bd40e3d7
SaneManiacMG <mmoroane@hotmail.com>
2023-10-25 14:00:05 +0200

    tutorial 20 for of and for in

---
+++ tutorial20.ts
@@ -0,0 +1,15 @@
+let colors = ["red", "green", "blue"];
+
+for(let index in colors) {
+    console.log(colors[index]);
+}
+
+for(let color of colors) {
+    console.log(color);
+}
+
+let letters = "abc";
+
+for(let letter of letters) {
+    console.log(letter);
+}
