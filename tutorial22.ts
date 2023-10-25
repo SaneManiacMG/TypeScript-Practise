65e432ce44b739337e552f4cada5f053d4040460
SaneManiacMG <mmoroane@hotmail.com>
2023-10-25 14:43:48 +0200

    tutorial 22 classes and methods

---
+++ tutorial22.ts
@@ -0,0 +1,19 @@
+class Person2 {
+  constructor(name) {
+    this.name = name;
+    console.log(this.name + " Constructor");
+  }
+
+  static staticMethod() {
+    console.log("Static Method");
+    
+  }
+  greetPerson2() {
+    console.log("Hello " + this.name);
+    
+  }
+}
+
+let p2 = new Person2("Chandler");
+Person2.staticMethod();
+p2.greetPerson2();
