3b0b6dfbcb23e614ed655a1bddf3fda020003bf9
SaneManiacMG <mmoroane@hotmail.com>
2023-10-25 15:41:26 +0200

    tutorial 26 named exports

---
+++ ESModules/src/moduleA.js
@@ -0,0 +1,7 @@
+console.log("moduleA.js: Log 1");
+import { fname as f, lname as l, obj } from "./moduleB.js";
+console.log("moduleA.js: Log 2");
+
+obj.name = "Ross";
+console.log(obj.name);
+console.log(`${f} ${l}`);
---
+++ ESModules/src/moduleB.js
@@ -0,0 +1,9 @@
+let fname = "Chandler";
+let lname = "Bing";
+
+let obj = {
+    name: "Joey"
+};
+
+console.log("moduleB.js: Loaded!");
+export { fname, lname, obj };
