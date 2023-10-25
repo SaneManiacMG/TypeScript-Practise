42036b40883004fec9a65029977aa0a91821bd14
SaneManiacMG <mmoroane@hotmail.com>
2023-10-25 13:49:11 +0200

    Destructuring objects

---
+++ tutorial18.ts
@@ -0,0 +1,13 @@
+let employee3 = {
+    fname2: "Chandler",
+    lname2: "Bing",
+    gender2: "Male"
+}
+
+let { fname2: f, lname2: l, gender2: g } = employee3;
+
+console.log(f);
+console.log(l);
+console.log(g);
+
+
